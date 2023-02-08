abdce2e6c9d7e660699b82ee1028dd91c140cf4b
Elvis Tavares <elvisptavares@gmail.com>
2023-02-07 21:50:49 -0300

    created database structure

---
+++ app/Http/Controllers/Reminder/ReminderController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Reminder;
+
+use App\Http\Controllers\Controller;
+use App\Models\Reminder;
+use Illuminate\Http\Request;
+
+class ReminderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        dd($request->all());
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Reminder  $reminder
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Reminder $reminder)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Reminder  $reminder
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Reminder $reminder)
+    {
+        //
+    }
+}
---
+++ database/migrations/2023_02_08_004046_create_reminder_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reminder', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('descReminder', 200);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reminder');
+    }
+};
